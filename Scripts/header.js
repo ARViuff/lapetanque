34ed268c9e2e7fac882fea729ddbf96dbcd26e21
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2021-12-14 10:33:43 +0100

    tilføjet header

---
+++ Scripts/header.js
@@ -0,0 +1,30 @@
+const hide = document.querySelector(".hide");
+const menuIcon = document.querySelector(".menuIcon");
+const menuIcon2 = document.querySelector(".menuIcon2");
+const slidermenu = document.querySelector(".slidermenu");
+
+menuIcon.addEventListener("click", () => {
+    slidermenu.classList.toggle("hide");
+    slidermenu.offsetLeft;
+    slidermenu.classList.toggle("hidded");
+  menuIcon.classList.toggle("gone");
+  menuIcon2.classList.toggle("gone");
+});
+menuIcon2.addEventListener("click", () => {
+    slidermenu.classList.toggle("hidded");
+    menuIcon.classList.toggle("gone");
+    menuIcon2.classList.toggle("gone");
+
+});
+
+menuIcon2.addEventListener("click", function (e) {
+    e.preventDefault;
+    
+    //introducing a delay of 40 seconds.
+    setTimeout(function () {
+        console.log("uyvgk")
+      slidermenu.classList.add("hide");
+    }, 40 * 9);
+  },
+  false
+);
